4d7644e3d26593b109b3f8434d173c5d961d73b6
Somesh Shukla <someshshuklacollege@gmail.com>
2025-04-22 13:29:18 +0530

    custom react folder

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from './chai'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>lllllllll</h1>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+  return (
+    <h3>Somesh aur chai</h3>
+  )
+}
+
+export default Chai
